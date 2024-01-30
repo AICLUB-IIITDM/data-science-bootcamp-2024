1aa15f5608483ed8a8c3d702f40222999d9a67a1
rahulsanagavarapu <cs21b2006@iiitdm.ac.in>
2024-01-30 13:39:27 +0530

    updated day07

--- day07/question.py
+++ day07/question.py
@@ -2,57 +2,40 @@ import numpy as np
 def create(list):
     # Create a numpy array from this list
     # Return the numpy array
-    my_array = np.array(list)
-    return my_array
     pass
 def convert(list):
-  #implement logic here
-  arr = np.array(list)
-  return arr.reshape(2, -1)
+  # Create a numpy array and Convert a 1D array to a 2D array with 2 rows
+  # Return the numpy array
   pass
 def common(a,b):
-  #implement logic here
-  a1 = np.array(a)
-  b1 = np.array(b)
-  return np.intersect1d(a1,b1)
+  #Get the common items between a and b
+  #Return the numpy array
   pass
 def match(a,b):
-  #implement logic here
-  a1 = np.array(a)
-  b1 = np.array(b)
-  return np.where(a1==b1)
+  #Get the positions/indices where elements of a and b match
+  #Return the numpy array
   pass
 def range(a):
-  #implement logic here
-  a = np.array(a)
-  return np.unique(a[(a >= 5) & (a <= 10)])
+  #Get all items between 5 and 10 from a.The items should be unique.
+  #Return the numpy array
   pass
 def swap(a):
-  #implement logic here
-  a = np.array(a)
-  a[:, [1, 0]] = a[:, [0, 1]]
-  return a
+  #Swap columns 1 and 2 in the 2D array
+  #Return the numpy array
   pass
 def reverse(arr):
-  #implement logic here
-  arr = np.array(arr)
-  arr = arr[::-1, :]
-  return arr
+  #Reverse the rows of a 2D array arr.
+  #Return the numpy array
   pass
 def replace(arr):
-  #implement logic here
-  arr = np.array(arr)
-  arr[arr % 2 == 1] = -1
-  return arr
+  #Replace all odd numbers in arr with -1
+  #Return the numpy array
   pass
 def extract(arr):
-  #implement logic here
-  arr = np.array(arr)
-  return arr[arr%2 == 1]
+  #Extract all odd numbers from array
+  #Return the numpy array
   pass
 def remove(a,b):
-  #implement logic here
-  a = np.array(a)
-  b = np.array(b)
-  return np.setdiff1d(a,b)
+  #From array 'a' remove all items present in array 'b'
+  #Return the numpy array
   pass
